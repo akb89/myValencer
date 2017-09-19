19d1f0d0465f5d3b774edb733d8d19a0a0770879
Corentin Ribeyre <cocophotos@orange.fr>
2017-09-20 01:15:42 +0200

    Import all config files to allow bundling the right configuration through envify

--- app/config/index.js
+++ app/config/index.js
@@ -1,10 +1,18 @@
 const baseConfig = require('./all');
 const _ = require('lodash');
+const production = require('./production');
+const development = require('./development');
+const testing = require('./testing');
 
 const env = process.env.NODE_ENV || 'development';
 let tmp;
 try {
-  tmp = require(`./${env}.js`); // eslint-disable-line
+    tmp = development;
+    if (env === 'production') {
+        tmp = production;
+    } else if (env === 'testing') {
+        tmp = testing;
+    }
 } catch (error) {
     // process.exit(1);
 }
