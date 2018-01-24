0517b0469abb901fac24399406b507f273c0edea
akb89 <akb@3azouz.net>
2018-01-24 21:23:12 +0100

    misc. cleanup

--- front/frontend/components/AnnotationSet/AnnotationSet.js
+++ front/frontend/components/AnnotationSet/AnnotationSet.js
@@ -6,11 +6,11 @@ module.exports = {
             if (!('valenceUnits' in pattern)) {
                 return '';
             }
-            return pattern.valenceUnits.reduce((array, val) => {
-                if (val.GF === undefined) {
-                    array.push(`${val.FE.name}.${val.PT}`);
+            return pattern.valenceUnits.reduce((array, vu) => {
+                if (vu.GF === undefined) {
+                    array.push(`${vu.FE.name}.${vu.PT}`);
                 } else {
-                    array.push(`${val.FE.name}.${val.PT}.${val.GF}`);
+                    array.push(`${vu.FE.name}.${vu.PT}.${vu.GF}`);
                 }
                 return array;
             }, []).join(' ');
@@ -19,14 +19,17 @@ module.exports = {
             const container = document.createElement('div');
             let offset = 0;
             const merged_labels = labels.reduce((filtered, label) => {
-                if (label.type === 'FE' && Object.prototype.hasOwnProperty.call(label, 'start') && Object.prototype.hasOwnProperty.call(label, 'end')) {
+                if (label.type === 'FE' &&
+                Object.prototype.hasOwnProperty.call(label, 'start') &&
+                Object.prototype.hasOwnProperty.call(label, 'end')) {
                     filtered.push(label);
-                } else if (label.type === 'Target' && Object.prototype.hasOwnProperty.call(label, 'start') && Object.prototype.hasOwnProperty.call(label, 'end')) {
+                } else if (label.type === 'Target' &&
+                Object.prototype.hasOwnProperty.call(label, 'start') &&
+                Object.prototype.hasOwnProperty.call(label, 'end')) {
                     filtered.push(label);
                 }
                 return filtered;
             }, []).sort((a, b) => a.start - b.start);
-            let j = 1;
             merged_labels.forEach((label) => {
                 const labeled_text = text.slice(label.start, label.end + 1);
                 const fragments = text.slice(offset, label.start).split('\n');
@@ -38,14 +41,18 @@ module.exports = {
                 });
                 if (label.type === 'FE') {
                     const mark = document.createElement('mark');
-                    mark.setAttribute('label-format', `f${j}`);
                     mark.setAttribute('fe-name', label.name);
+                    if (label.name in this.$store.state.annoset.feColorMap) {
+                        mark.setAttribute('label-format', this.$store.state.annoset.feColorMap[label.name]);
+                    } else {
+                        this.$store.commit('annoset/add_entry_to_map', { feName: label.name, color: `f${this.$store.state.annoset.colorIndex}` });
+                        mark.setAttribute('label-format', `f${this.$store.state.annoset.colorIndex}`);
+                    }
                     mark.appendChild(document.createTextNode(labeled_text));
                     container.appendChild(mark);
-                    j += 1;
                 } else if (label.type === 'Target') {
                     const mark = document.createElement('mark');
-                    mark.setAttribute('label-format', 'f8');
+                    mark.setAttribute('label-format', 'f0');
                     mark.appendChild(document.createTextNode(labeled_text));
                     container.appendChild(mark);
                 }
